643897da40618c4e2663836bcc0010f2f7d2c1ec
JavierAmdR <javieramador_bcnvj1ma1920@cev.com>
2020-11-15 21:43:51 +0100

    Commit inicial

---
+++ Editor/EditorPsious.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class EditorPsious : EditorWindow
+{
+    string currentLine;
+    string path;
+    string currentFolder;
+
+    [MenuItem("Window/EditorPsious")]
+    public static void ShowWindow() 
+    {
+        GetWindow<EditorPsious>("EditorPsious");
+    }
+    private void OnGUI()
+    {
+        if (GUILayout.Button("CheckFiles")) 
+        {
+            
+            string filePath = "Packages/com.javieramador.editorpsious/Resources/DirectoriesPsious.txt";
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            for (int i = 0; i < lines.Length; i++) 
+            {
+                currentLine = lines[i];
+                if (currentLine[0] != '\t') 
+                {
+                    currentFolder = lines[i] + "/";
+                    if (!Directory.Exists(path)) 
+                    {
+                        Debug.LogWarning("Error " + currentFolder + " no existe");
+                    }
+                }
+                else 
+                {
+                    path = currentFolder + lines[i].Trim() + "/";
+                    if (!Directory.Exists(path)) 
+                    {
+                        Debug.LogWarning("Error " + path + " no existe");
+                    }
+                }
+            }
+        }
+    }
+}
